fc2eb9601c57630fa251eb76ef4b6dc782983afa
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-06 19:27:03 +0330

    Debug: singleton_violate_multithread test

--- Python/Creational/Singleton/test/singleton_violating_multithread.py
+++ Python/Creational/Singleton/test/singleton_violating_multithread.py
@@ -2,7 +2,7 @@ from threading import Lock
 import time
 
 
-class SingletonPatternThreadSafe(type):
+class SingletonPatternThreadSafeToViolate(type):
     _instances = {}
 
     # _lock: Lock = Lock()
@@ -12,10 +12,7 @@ class SingletonPatternThreadSafe(type):
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
             value = kwargs.get('value', None)
-            if value:
-                print ('instance {value} created')
             if value == 2:
-                print('thread 2 waiting 2 seconds to assingg the instance in cls._instances[cls] ...')
                 time.sleep(2)
             cls._instances[cls] = instance
         
