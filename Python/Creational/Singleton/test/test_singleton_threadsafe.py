4b4af05adf4ec1c554f25a50e726ddb1e1da7123
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-06 19:17:58 +0330

    test_singleton_threadsafe

---
+++ Python/Creational/Singleton/test/test_singleton_threadsafe.py
@@ -0,0 +1,65 @@
+from unittest import TestCase
+import unittest
+
+import pathlib
+import sys
+path = str(pathlib.Path(__file__).parent.parent.absolute())
+sys.path.append(path)
+
+from threading import Thread
+from singleton_thread_safe import SingletonPatternThreadSafe
+
+
+class SingletonPatternTestCase(TestCase):
+    _FIRST_FOO_VALUE = "FOO"
+    
+    def setUp(self):
+        class BlahBlah(metaclass=SingletonPatternThreadSafe):
+            pass
+        class BlahBlahChild(BlahBlah):
+            pass
+        
+        self.blah_1 = BlahBlah()
+        self.blah_2 = BlahBlah()
+        self.blah_child_1 = BlahBlahChild()
+        self.blah_child_2 = BlahBlahChild()
+        
+        class Foo(metaclass=SingletonPatternThreadSafe):
+            def __init__(self, value):
+                self.value = value
+        
+        self.Foo = Foo
+    
+    def tearDown(self):
+        pass
+    
+    def _test_singleton(self, value):
+        foo = self.Foo(value=value)
+        self.foo_id = id(foo)
+    
+    def test_singleton_with_thread(self):
+        # TODO: Prove thread_conflict can cause create more than one instance with an example 
+        
+        process1 = Thread(target=self._test_singleton, args=(self._FIRST_FOO_VALUE,))
+        process2 = Thread(target=self._test_singleton, args=("BAR",))
+        process1.start()
+        foo_id_1 = int(self.foo_id)
+        process2.start()
+        foo_id_2 = int(self.foo_id)
+        self.assertEqual(foo_id_1, foo_id_2)
+    
+    def test_singleton_without_thread(self):
+        self.assertEqual(self.blah_1, self.blah_2)
+    
+    def test_singleton_for_childs_without_thread(self):
+        self.assertEqual(self.blah_child_1, self.blah_child_2)
+    
+    def test_not_equal_different_classes_without_thread(self):
+        self.assertNotEqual(self.blah_1, self.Foo(value='BLAH'))
+    
+    def test_not_equal_parent_and_child_without_thread(self):
+        self.assertNotEqual(self.blah_1, self.blah_child_1)
+
+
+if __name__=='__main__':
+    unittest.main()
