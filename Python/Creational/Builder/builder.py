0f7cf3265e153039709c488df5e5f4647fe01c8b
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-09 17:25:23 +0330

    Add: Builder pattern

---
+++ Python/Creational/Builder/builder.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any
+
+# Code from: "https://refactoring.guru/design-patterns/builder/python/example"
+
+
+class AbstractBuilder(ABC):
+
+    @property
+    @abstractmethod
+    def product(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_a(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_b(self) -> None:
+        pass
+
+    @abstractmethod
+    def produce_part_c(self) -> None:
+        pass
+
+
+class ConcreteBuilder(AbstractBuilder):
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = Product()
+
+    @property
+    def product(self) -> Product:
+        product = self._product
+        self.reset()
+        return product
+
+    def produce_part_a(self) -> None:
+        self._product.add("PartA1")
+
+    def produce_part_b(self) -> None:
+        self._product.add("PartB1")
+
+    def produce_part_c(self) -> None:
+        self._product.add("PartC1")
+
+
+class Product():
+    
+    def __init__(self) -> None:
+        self.parts = []
+
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+
+    @property
+    def list_parts(self) -> str:
+        return f"Product parts: {', '.join(self.parts)}\n"
+
+
+class Director:
+
+    def __init__(self) -> None:
+        self._builder = None
+
+    @property
+    def builder(self) -> AbstractBuilder:
+        return self._builder
+
+    @builder.setter
+    def builder(self, builder: AbstractBuilder) -> None:
+        self._builder = builder
+
+    def build_minimal_viable_product(self) -> None:
+        self.builder.produce_part_a()
+
+    def build_full_featured_product(self) -> None:
+        self.builder.produce_part_a()
+        self.builder.produce_part_b()
+        self.builder.produce_part_c()
+
+
+if __name__ == "__main__":
+    
+    director = Director()
+    builder = ConcreteBuilder()
+    director.builder = builder
+
+    print("Standard basic product: ")
+    director.build_minimal_viable_product()
+    print(builder.product.list_parts) #PartA1
+
+    print("Standard full featured product: ")
+    director.build_full_featured_product()
+    print(builder.product.list_parts) #PartA1, PartB1, PartC1
+
+    # Remember, the Builder pattern can be used without a Director class.
+    print("Custom product: ")
+    builder.produce_part_a()
+    builder.produce_part_b()
+    print(builder.product.list_parts) #PartA1, PartB1
