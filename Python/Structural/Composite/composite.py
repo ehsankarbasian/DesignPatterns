05475fd85b995f1541c52ea718a444ee33038535
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-23 14:08:35 +0330

    print composite structure indented

--- Python/Structural/Composite/composite.py
+++ Python/Structural/Composite/composite.py
@@ -1,4 +1,14 @@
 from abc import ABC, abstractmethod
+import sys
+
+_indent_level = 0
+
+def _print_indented(text):
+    global _indent_level
+    text = _indent_level*'    ' + text
+    sys.stdout.write(text+"\n")
+
+print = _print_indented
 
 
 class GraphicInterface(ABC):
@@ -10,8 +20,12 @@ class GraphicInterface(ABC):
     @abstractmethod
     def draw(self):
         pass
+    
+    def __str__(self):
+        return f'{self.__class__.__name__} (id:{str(id(self))[-5:]})'
 
 
+# Leaf
 class Dot(GraphicInterface):
     
     def __init__(self, x, y):
@@ -23,9 +37,10 @@ class Dot(GraphicInterface):
         self.y += y
     
     def draw(self):
-        print(f'Drawing Dot ({self.x}, {self.y})')
+        print(f'Drawing {self} ({self.x}, {self.y})')
 
 
+# Leaf
 class Circle(Dot):
     
     def __init__(self, x, y, radius):
@@ -33,9 +48,10 @@ class Circle(Dot):
         super().__init__(x, y)
     
     def draw(self):
-        print(f'Drawing Circle ({self.x}, {self.y}, {self.radius})')
+        print(f'Drawing {self} ({self.x}, {self.y}, {self.radius})')
 
 
+# NOT Leaf
 class CompoundGraphic(GraphicInterface):
     
     def __init__(self):
@@ -52,32 +68,41 @@ class CompoundGraphic(GraphicInterface):
             child.move(x, y)
     
     def draw(self):
+        global _indent_level
+        print(f'--> Begin draw {self}')
+        _indent_level += 1
+        
         for child in self.__children:
             child.draw()
             # Update the bounding rectangle.
         # Draw a dashed rectangle using the bounding coordinates
+        
+        _indent_level -= 1
+        print(f'--> End draw {self}')
 
 
 class ImageEditor:
     
     def __init__(self):
-        self.all = CompoundGraphic()
+        # Composite structure root
+        self.root = CompoundGraphic()
     
     def add_shape(self, obj):
-        self.all.add(obj)
+        self.root.add(obj)
     
     def group_selected(self, components):
+        # Composite structure SubBranch
+        # group in neither Leaf nor Root
         _group = CompoundGraphic()
         
         for component in components:
             _group.add(component)
-            self.all.remove(component)
+            self.root.remove(component)
         
-        self.all.add(_group)
-
+        self.root.add(_group)
     
     def _draw_all(self):
-        self.all.draw()
+        self.root.draw()
 
 
 editor = ImageEditor()
