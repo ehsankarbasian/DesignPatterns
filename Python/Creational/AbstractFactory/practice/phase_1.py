9d336940eaffa890ab3154621a3eed755b1f7701
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-09-01 20:09:46 +0330

    AbstractFactory practice phase_1 Add concrete factories and product classes (functions not implemented yet)

--- Python/Creational/AbstractFactory/practice/phase_1.py
+++ Python/Creational/AbstractFactory/practice/phase_1.py
@@ -8,9 +8,9 @@ Implement products [
     Camera
 ]
 for the brands [
-    LG,
     Samsung,
     Sony
+    LG,
 ]
 '''
 
@@ -31,6 +31,54 @@ class ProductFactory(ABC):
         pass
     
     @abstractmethod
+    def make_camera(self):
+        pass
+
+
+# Concrete Factory (SAMSUNG)
+class SamaungFactory(ProductFactory):
+    
+    def make_tv(self):
+        pass
+    
+    def make_radio(self):
+        pass
+    
+    def make_phone(self):
+        pass
+    
+    def make_camera(self):
+        pass
+
+
+# Concrete Factory (SONY)
+class SonyFactory(ProductFactory):
+    
+    def make_tv(self):
+        pass
+    
+    def make_radio(self):
+        pass
+    
+    def make_phone(self):
+        pass
+    
+    def make_camera(self):
+        pass
+
+
+# Concrete Factory (LG)
+class LgFactory(ProductFactory):
+    
+    def make_tv(self):
+        pass
+    
+    def make_radio(self):
+        pass
+    
+    def make_phone(self):
+        pass
+    
     def make_camera(self):
         pass
 
@@ -134,5 +182,450 @@ class AbstractCamera(AbstractBaseProduct):
         pass
     
     @abstractmethod
+    def zoom_out(self):
+        pass
+
+
+# Concrete Products (TV)
+class SamsungTv(AbstractTV):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def channel_up(self):
+        pass
+    
+    def channel_down(self):
+        pass
+    
+    def volume_up(self):
+        pass
+    
+    def volume_down(self):
+        pass
+    
+    def mute(self):
+        pass
+    
+    def unmute(self):
+        pass
+
+    def increase_screen_brightness(self):
+        pass
+    
+    def decrease_screen_brightness(self):
+        pass
+
+
+class SonyTv(AbstractTV):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def channel_up(self):
+        pass
+    
+    def channel_down(self):
+        pass
+    
+    def volume_up(self):
+        pass
+    
+    def volume_down(self):
+        pass
+    
+    def mute(self):
+        pass
+    
+    def unmute(self):
+        pass
+
+    def increase_screen_brightness(self):
+        pass
+    
+    def decrease_screen_brightness(self):
+        pass
+
+
+class LgTv(AbstractTV):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def channel_up(self):
+        pass
+    
+    def channel_down(self):
+        pass
+    
+    def volume_up(self):
+        pass
+    
+    def volume_down(self):
+        pass
+    
+    def mute(self):
+        pass
+    
+    def unmute(self):
+        pass
+
+    def increase_screen_brightness(self):
+        pass
+    
+    def decrease_screen_brightness(self):
+        pass
+
+
+# Concrete Products (Radio)
+class SamsungRadio(AbstractRadio):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def channel_up(self):
+        pass
+    
+    def channel_down(self):
+        pass
+    
+    def volume_up(self):
+        pass
+    
+    def volume_down(self):
+        pass
+    
+    def mute(self):
+        pass
+    
+    def unmute(self):
+        pass
+
+    def set_mode(self, mode):
+        pass
+
+
+class SonyRadio(AbstractRadio):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def channel_up(self):
+        pass
+    
+    def channel_down(self):
+        pass
+    
+    def volume_up(self):
+        pass
+    
+    def volume_down(self):
+        pass
+    
+    def mute(self):
+        pass
+    
+    def unmute(self):
+        pass
+
+    def set_mode(self, mode):
+        pass
+
+
+class LgRadio(AbstractRadio):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def channel_up(self):
+        pass
+    
+    def channel_down(self):
+        pass
+    
+    def volume_up(self):
+        pass
+    
+    def volume_down(self):
+        pass
+    
+    def mute(self):
+        pass
+    
+    def unmute(self):
+        pass
+
+    def set_mode(self, mode):
+        pass
+
+
+# Concrete Products (Phone)
+class SamsungPhone(AbstractPhone):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def call(self, number):
+        pass
+    
+    def reject_call(self):
+        pass
+
+
+class SonyPhone(AbstractPhone):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def call(self, number):
+        pass
+    
+    def reject_call(self):
+        pass
+
+
+class LgPhone(AbstractPhone):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def call(self, number):
+        pass
+    
+    def reject_call(self):
+        pass
+
+
+# Concrete Products (Camera)
+class SamsungCamera(AbstractCamera):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def take_picture(self):
+        pass
+    
+    def take_film(self):
+        pass
+    
+    def zoom_in(self):
+        pass
+    
+    def zoom_out(self):
+        pass
+
+
+class SonyCamera(AbstractCamera):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def take_picture(self):
+        pass
+    
+    def take_film(self):
+        pass
+    
+    def zoom_in(self):
+        pass
+    
+    def zoom_out(self):
+        pass
+
+
+class LgCamera(AbstractCamera):
+    
+    @property
+    def weight(self):
+        pass
+    
+    @property
+    def average_life_span(self):
+        pass
+    
+    @property
+    def warranty_expiration_date(self):
+        pass
+    
+    def turn_on(self):
+        pass
+    
+    def turn_off(self):
+        pass
+
+    def take_picture(self):
+        pass
+    
+    def take_film(self):
+        pass
+    
+    def zoom_in(self):
+        pass
+    
     def zoom_out(self):
         pass
