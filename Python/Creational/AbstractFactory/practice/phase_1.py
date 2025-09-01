65546c4d25c49b6e8bead41c0ee073e39f0b4416
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-09-01 20:08:53 +0330

    AbstractFactory practice phase_1 Add abstract factory and product classes

--- Python/Creational/AbstractFactory/practice/phase_1.py
+++ Python/Creational/AbstractFactory/practice/phase_1.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 '''
 Implement products [
     TV,
@@ -11,3 +13,126 @@ for the brands [
     Sony
 ]
 '''
+
+
+# Abstract Factory
+class ProductFactory(ABC):
+    
+    @abstractmethod
+    def make_tv(self):
+        pass
+    
+    @abstractmethod
+    def make_radio(self):
+        pass
+    
+    @abstractmethod
+    def make_phone(self):
+        pass
+    
+    @abstractmethod
+    def make_camera(self):
+        pass
+
+
+# Not a part of the pattern, just to DRY
+class AbstractBaseProduct(ABC):
+    @property
+    @abstractmethod
+    def weight(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def average_life_span(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def warranty_expiration_date(self):
+        pass
+    
+    @abstractmethod
+    def turn_on(self):
+        pass
+    
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+
+# Not a part of the pattern, just to DRY
+class AbstractMediaPlayerProduct(ABC):
+    
+    @abstractmethod
+    def channel_up(self):
+        pass
+    
+    @abstractmethod
+    def channel_down(self):
+        pass
+    
+    @abstractmethod
+    def volume_up(self):
+        pass
+    
+    @abstractmethod
+    def volume_down(self):
+        pass
+    
+    @abstractmethod
+    def mute(self):
+        pass
+    
+    @abstractmethod
+    def unmute(self):
+        pass
+
+
+# Abstract Products (TV, Radio, Phone, Camera)
+class AbstractTV(AbstractBaseProduct, AbstractMediaPlayerProduct):
+    
+    @abstractmethod
+    def increase_screen_brightness(self):
+        pass
+    
+    @abstractmethod
+    def decrease_screen_brightness(self):
+        pass
+
+
+class AbstractRadio(AbstractBaseProduct, AbstractMediaPlayerProduct):
+    
+    @abstractmethod
+    def set_mode(self, mode):
+        pass
+
+
+class AbstractPhone(AbstractBaseProduct):
+    
+    @abstractmethod
+    def call(self, number):
+        pass
+    
+    @abstractmethod
+    def reject_call(self):
+        pass
+
+
+class AbstractCamera(AbstractBaseProduct):
+    
+    @abstractmethod
+    def take_picture(self):
+        pass
+    
+    @abstractmethod
+    def take_film(self):
+        pass
+    
+    @abstractmethod
+    def zoom_in(self):
+        pass
+    
+    @abstractmethod
+    def zoom_out(self):
+        pass
