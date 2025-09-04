f26edee3d9cec7d9330521163082dd7103599ddf
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-09-04 16:33:53 +0330

    Add onother publisher to Observer better_example

--- Python/Behavioral/Observer/better_example.py
+++ Python/Behavioral/Observer/better_example.py
@@ -4,18 +4,26 @@ from abc import ABC, abstractmethod
 # Subject interface
 class PublisherInterface(ABC):
     
+    @abstractmethod
     def register(self, subscriber):
         pass
     
+    @abstractmethod
     def deregister(self, subscriber):
         pass
     
+    @abstractmethod
     def notify_subscribers(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def latest_not_notified(self):
         pass
 
 
-# Concrete subject
-class Publisher(PublisherInterface):
+# Concrete subject 1
+class PublisherBook(PublisherInterface):
 
     def __init__(self):
         self.__subscribers = []
@@ -32,10 +40,10 @@ class Publisher(PublisherInterface):
         for subscriber in self.__subscribers:
             subscriber.update(self)
         self._clear_latest_books()
-
+    
     def _clear_latest_books(self):
         self.__latest_books = []
-        
+
     def add_book(self, book):
         self.__all_books.append(book)
         self.__latest_books.append(book)
@@ -45,8 +53,47 @@ class Publisher(PublisherInterface):
         return self.__all_books
     
     @property
-    def latest_books(self):
+    def latest_not_notified(self):
         return self.__latest_books
+    
+    @property
+    def subscribers(self):
+        return [type(s).__name__ for s in self.__subscribers]
+
+
+# Concrete subject 2
+class PublisherArticle(PublisherInterface):
+    
+    def __init__(self):
+        self.__subscribers = []
+        self.__latest_articles = []
+        self.__all_articles = []
+
+    def register(self, subscriber):
+        self.__subscribers.append(subscriber)
+
+    def deregister(self, subscriber):
+        self.__subscribers.remove(subscriber)
+
+    def notify_subscribers(self):
+        for subscriber in self.__subscribers:
+            subscriber.update(self)
+        self._clear_latest_articles()
+    
+    def _clear_latest_articles(self):
+        self.__latest_articles = []
+
+    def add_article(self, article):
+        self.__all_articles.append(article)
+        self.__latest_articles.append(article)
+
+    @property
+    def all_articles(self):
+        return self.__all_articles
+    
+    @property
+    def latest_not_notified(self):
+        return self.__latest_articles
     
     @property
     def subscribers(self):
@@ -65,41 +112,65 @@ class SubscriberInterface(ABC):
 class SMSSubscriber(SubscriberInterface):
 
     def update(self, subject):
-        print("Update from:", type(subject).__name__, subject.latest_books)
-
+        publisher = type(subject).__name__
+        subscriber = type(self).__name__
+        print(f'Update from "{publisher}" to "{subscriber}":')
+        print(subject.latest_not_notified, '\n')
 
 # Concrete Observer 2
 class EmailSubscriber(SubscriberInterface):
 
     def update(self, subject):
-        print("Update from:", type(subject).__name__, subject.latest_books)
+        publisher = type(subject).__name__
+        subscriber = type(self).__name__
+        print(f'Update from "{publisher}" to "{subscriber}":')
+        print(subject.latest_not_notified, '\n')
 
 
 # Concrete Observer 3
 class AnyOtherSubscriber(SubscriberInterface):
 
     def update(self, subject):
-        print("Update from:", type(subject).__name__, subject.latest_books)
-
+        publisher = type(subject).__name__
+        subscriber = type(self).__name__
+        print(f'Update from "{publisher}" to "{subscriber}":')
+        print(subject.latest_not_notified, '\n')
 
 if __name__ == '__main__':
     sms_subscriber = SMSSubscriber()
     email_subscriber = EmailSubscriber()
     other_subscriber = AnyOtherSubscriber()
     
-    publisher = Publisher()
-    publisher.register(sms_subscriber)
-    publisher.register(email_subscriber)
-    publisher.register(other_subscriber)
+    # Use book publisher
+    publisher_book = PublisherBook()
+    publisher_book.register(sms_subscriber)
+    publisher_book.register(email_subscriber)
+    publisher_book.register(other_subscriber)
+    print("\nPublisherBook Subscribers: ", publisher_book.subscribers)
     
-    print("\nSubscribers: ", publisher.subscribers)
-    publisher.add_book("Advanced Python")
-    publisher.notify_subscribers()
+    publisher_book.add_book("Advanced Python")
+    publisher_book.notify_subscribers()
     
-    publisher.deregister(email_subscriber)
-    print("\nUnregistered: EmailSubscriber")
-    print("\nSubscribers: ", publisher.subscribers)
+    publisher_book.deregister(email_subscriber)
+    print("\nnPublisherBook unregistered: EmailSubscriber")
+    print("\nPublisherBook Subscribers: ", publisher_book.subscribers)
     
-    publisher.add_book("Clean Code")
-    publisher.add_book("The Clean Coder")
-    publisher.notify_subscribers()
+    publisher_book.add_book("Clean Code")
+    publisher_book.notify_subscribers()
+    
+    # Use article publisher
+    publisher_article = PublisherArticle()
+    publisher_article.register(sms_subscriber)
+    publisher_article.register(email_subscriber)
+    print("\nPublisherArticle Subscribers: ", publisher_article.subscribers)
+    
+    publisher_article.add_article("Real Python")
+    publisher_article.add_article("Thricks")
+    publisher_article.notify_subscribers()
+    
+    publisher_article.deregister(sms_subscriber)
+    print("\nPublisherArticle unregistered: SmsSubscriber")
+    publisher_article.register(other_subscriber)
+    print("\nPublisherArticle Subscribers: ", publisher_article.subscribers)
+    publisher_article.add_article("Design patterns")
+    publisher_article.notify_subscribers()
